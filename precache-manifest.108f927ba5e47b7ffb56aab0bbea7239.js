22c0103ff9085a9e041ba67fea605d0a50d0203f
Shubham Panchal <shubhampanchal9773@gmail.com>
2021-02-10 19:27:34 +0530

    Updates

---
+++ precache-manifest.108f927ba5e47b7ffb56aab0bbea7239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b6b4776daaafe2656a1ecacd18b750",
+    "url": "/room-deck/index.html"
+  },
+  {
+    "revision": "46ed7635f5f351a4807b",
+    "url": "/room-deck/static/css/main.fc98c4cb.chunk.css"
+  },
+  {
+    "revision": "fcfa247ee6c6a27cfcc3",
+    "url": "/room-deck/static/js/2.b09a7d08.chunk.js"
+  },
+  {
+    "revision": "e106d564111f23746c5f19c65cff94db",
+    "url": "/room-deck/static/js/2.b09a7d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ed7635f5f351a4807b",
+    "url": "/room-deck/static/js/main.7a16685f.chunk.js"
+  },
+  {
+    "revision": "209be2b22ee9324dac1c",
+    "url": "/room-deck/static/js/runtime-main.a11c14bb.js"
+  }
+]);
